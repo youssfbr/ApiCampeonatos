29898f7d2a1aea5f5318c46b44d4ed1d9a4f686d
Alisson Youssf <youssfbr@gmail.com>
2021-09-04 01:56:00 -0300

    Controller

---
+++ ApiCampeonatos/Controllers/CampeonatoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiCampeonatos.Data;
+
+namespace ApiCampeonatos.Controllers
+{
+    [Route("[controller]/classificacao_geral")]
+    [ApiController]
+    public class CampeonatoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CampeonatoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Campeonato>> GetCampeonato(int id)
+        {
+            var campeonato = await _context.Campeonatos.FindAsync(id);
+
+            if (campeonato == null)
+            {
+                return NotFound();
+            }
+
+            return campeonato;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Campeonato>> PostCampeonato(Campeonato campeonato)
+        {         
+            _context.Campeonatos.Add(campeonato);
+            await _context.SaveChangesAsync();
+          
+            return CreatedAtAction("GetCampeonato", new { id = campeonato.Id }, campeonato);
+        }
+    }
+}
