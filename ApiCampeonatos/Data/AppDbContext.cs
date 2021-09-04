0ee8e137ff7994cfb85234f72323b9a7a27a6da7
Alisson Youssf <youssfbr@gmail.com>
2021-09-04 00:52:47 -0300

    DbContext

---
+++ ApiCampeonatos/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCampeonatos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :  base(options) { }
+                
+        public DbSet<Jogo> Jogos { get; set; }
+        public DbSet<Campeonato> Campeonatos { get; set; }        
+    }
+}
