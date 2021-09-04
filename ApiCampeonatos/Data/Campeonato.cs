3d69c7aa9c88b4308c9a11dd00c3188ef60f2eda
Alisson Youssf <youssfbr@gmail.com>
2021-09-04 00:40:25 -0300

    Entities: Campeonato, Jogo

---
+++ ApiCampeonatos/Data/Campeonato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCampeonatos.Data
+{
+    public class Campeonato
+    {
+        public int Id { get; set; }
+        public IEnumerable<Jogo> Jogadas { get; set; }
+    }
+}
---
+++ ApiCampeonatos/Data/Jogo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCampeonatos.Data
+{
+    public class Jogo
+    {
+        public int Id { get; set; }
+        public string Jogada { get; set; }
+        public string Resultado { get; set; }
+    }
+}
